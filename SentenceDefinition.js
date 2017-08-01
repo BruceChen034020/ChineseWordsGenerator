3892e20bf6e9319e0e5ab33e3d93244c8ba93d86
BruceChen034020 <2017greatplatform@gmail.com>
2017-08-01 20:28:11 +0800

    Add files via upload

--- SentenceDefinition.js
+++ SentenceDefinition.js
@@ -25,13 +25,62 @@ function SentenceDefinition(){
     }
     this.showRandomDefinition = function(){
         definitionTextBox.innerHTML += "釋義: ";
+        var s = this.produceRandomDefinition();
+        t = this.processRandomDefinition(s);
+        definitionTextBox.innerHTML += t;
+    }
+    this.produceRandomDefinition = function(){
+        var s = "";
         for(var i = 0; i<self.parent.words.length; i++){
             var v = Definition[self.parent.words[i].wordID];
             var r = Math.random();
             r *= v.length;
             r = Math.floor(r);
-            var s = v[r];
-            definitionTextBox.innerHTML += s;
+            if(typeof(v[r]) !== 'undefined')
+                s += v[r];
         }
+        return s;
+    }
+    this.processRandomDefinition = function(s){
+        var t = "";
+        var r2 = Math.random();
+        console.log("prefix.length: " + prefix.length);
+        r2 *= prefix.length;
+        r2 = Math.floor(r2);
+        t += prefix[r2];
+        for(var i=0; i<s.length; i++){
+            var c = s[i];
+            if(c === '。'){
+                var r = Math.random();
+                if(this.parent.words.length<7) {
+                    if (r < 1 / 3) {
+                        c = '之';
+                    } else {
+                        c = '';
+                    }
+                }else{
+                    if(r < 1/7){
+                        var r4 = Math.random();
+                        r4 *= connection.length;
+                        r4 = Math.floor(r4);
+                        t += connection[r4];
+                    }
+                    c = '';
+                }
+            }
+            if(c ==  '('){
+                if(s[i+1] == ')'){
+                    c = '';
+                    i++;
+                }
+            }
+            t += c;
+            pre = c;
+        }
+        var r3 = Math.random();
+        r3 *= suffix.length;
+        r3 = Math.floor(r3);
+        t += suffix[r3];
+        return t;
     }
 }
---
+++ suffix.js
@@ -0,0 +1,104 @@
+var prefix = [];
+var suffix = [];
+var connection = [];
+
+function prefix_initialization(){
+    prefix[0] = "嗨！ ";
+    prefix[1] = "蛤？";
+    prefix[2] = "哦？";
+    prefix[3] = "呃......";
+    prefix[4] = "";
+    prefix[5] = "";
+    prefix[6] = "";
+    prefix[7] = "";
+    prefix[8] = "";
+    prefix[9] = "";
+    prefix[10] = "";
+    prefix[11] = "誒？";
+    prefix[12] = "欸！";
+    prefix[13] = "唉！";
+    prefix[14] = "嗯......";
+    prefix[15] = "噢！";
+    prefix[16] = "哇！";
+    prefix[17] = "鏘鏘鏘~~~";
+}
+
+function suffix_initialization(){
+    suffix[0] = "了。";
+    suffix[1] = "啦！";
+    suffix[2] = "喔！";
+    suffix[3] = "呢！";
+    suffix[4] = "呢？";
+    suffix[5] = "了。";
+    suffix[6] = "了。";
+    suffix[7] = "了。";
+    suffix[8] = "了。";
+    suffix[9] = "了。";
+    suffix[10] = "了。";
+    suffix[11] = "啦！";
+    suffix[12] = "啦！";
+    suffix[13] = "啦！";
+    suffix[14] = "啦！";
+    suffix[15] = "啦！";
+    suffix[16] = "啦！";
+    suffix[17] = "喔！";
+    suffix[18] = "喔！";
+    suffix[19] = "喔！";
+    suffix[20] = "喔！";
+    suffix[21] = "喔！";
+    suffix[22] = "喔！";
+    suffix[23] = "捏！";
+    suffix[24] = "嘍！";
+    suffix[25] = "咧！";
+    suffix[26] = "吧！";
+    suffix[27] = "嗎？";
+    suffix[28] = "呵！";
+    suffix[29] = "齁！";
+    suffix[30] = "XD";
+    suffix[31] = "哼！";
+    suffix[32] = "蛤？";
+    suffix[33] = "。醬。";
+    suffix[34] = "。嘻！";
+    suffix[35] = "OAO";
+    suffix[36] = "砟！";
+    suffix[37] = "嘖！";
+    suffix[38] = "，切！";
+    suffix[39] = "唔？";
+    suffix[40] = "嗚.......";
+    suffix[41] = "啊！";
+    suffix[42] = "哦？";
+    suffix[43] = "。誒？";
+    suffix[44] = "。欸！";
+    suffix[45] = "唉！";
+    suffix[46] = "哩！";
+    suffix[47] = "QQ";
+    suffix[48] = "QAQ";
+    suffix[49] = "。喵！";
+    suffix[50] = "w";
+    suffix[51] = "www";
+    suffix[52] = "呀！";
+    suffix[53] = "。哈哈哈。";
+    suffix[54] = "。笑死！";
+    suffix[55] = "。棒！";
+    suffix[56] = "...";
+    suffix[54] = "。Bang!";
+    suffix[55] = "。";
+    suffix[56] = "了！";
+    suffix[57] = "！";
+    suffix[58] = "啊~";
+    suffix[59] = "(*^^*)";
+    suffix[60] = "囉~";
+    suffix[61] = "ㄏㄏ";
+    suffix[62] = "啊 (茶";
+    suffix[63] = " (笑";
+    suffix[64] = "。賺！";
+    suffix[65] = "哪~~~~";
+    suffix[66] = " = =";
+}
+
+function connection_initialization(){
+    connection[0] = "，因為";
+    connection[1] = "，";
+    connection[2] = "，然後";
+    connection[3] = "，所以";
+}
